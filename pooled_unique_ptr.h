f2d2f5bdbd43f833fd64522a7c784edaf2bd2c86
Jorge Lopez <jorgext@gmail.com>
2022-02-27 19:16:26 +0100

    setup pooled unique ptr

---
+++ pooled_unique_ptr.h
@@ -0,0 +1,142 @@
+/**
+ * @file pooled_unique_ptr.h
+ * @author Jorge Lopez
+ * @brief A quick and dirty implementation of a pooled unique pointer.
+ * @version 0.1
+ * @date 2022-02-26
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+#include <bitset>
+#include <map>
+#include <utility>
+#include <stdexcept>
+#include <unordered_set>
+
+#define MAX_POOL_SIZE 4
+
+template <typename T>
+class pooled_unique_ptr
+{
+private:
+    T *_ptr;
+    size_t _ptr_pool_index; // position in the pool
+
+    static inline bool _initialized = false;
+    static inline size_t _pool_pos;                             // pointer to the next free position in the pool
+    static inline char _pool_buffer[sizeof(T) * MAX_POOL_SIZE]; // TODO: check the type of the buffer
+    static inline std::bitset<MAX_POOL_SIZE> _pool_slots;
+    static inline std::unordered_set<size_t> _pool_open_slots; // keep track of O(1) average access/insert/delete
+    size_t find_open_slot();
+
+public:
+    pooled_unique_ptr() {}
+
+    template <typename... Args>
+    pooled_unique_ptr(Args &&...args)
+    {
+        // _ptr = new T(std::forward<Args>(args)...);
+        if (!_initialized)
+        {
+            initialize_pool();
+        }
+
+        printf("current mmap (before allocation): %s\n", _pool_slots.to_string().c_str());
+        auto pos = find_open_slot();
+        printf("allocated slot: %zu\n", pos);
+        _ptr = new (_pool_buffer + sizeof(T) * pos) T(std::forward<Args>(args)...);
+        _ptr_pool_index = pos;
+        _pool_slots.set(pos);
+        printf("current mmap (after allocation): %s\n", _pool_slots.to_string().c_str());
+    }
+
+    ~pooled_unique_ptr()
+    {
+        printf("current mmap (before deallocation): %s \t deallocating slot %ld \n", _pool_slots.to_string().c_str(), _ptr_pool_index);
+        if (_ptr != nullptr)
+        {
+            _pool_slots.set(_ptr_pool_index, false);
+            _pool_open_slots.insert(_ptr_pool_index);
+        }
+        printf("current mmap (after deallocation): %s\n", _pool_slots.to_string().c_str());
+        for (const auto &slot : _pool_open_slots)
+        {
+            printf("slot %zu is open\n", slot);
+        }
+    }
+
+    // copy constructor - disabled
+    pooled_unique_ptr(const pooled_unique_ptr &) = delete;
+
+    // move constructor
+    pooled_unique_ptr(pooled_unique_ptr &&other)
+    {
+        _ptr = std::move(other._ptr);
+        _ptr_pool_index = other._ptr_pool_index;
+        other._ptr = nullptr;
+    };
+
+    // copy assignment - disabled
+    pooled_unique_ptr &operator=(const pooled_unique_ptr &) = delete;
+
+    // move assignment
+    pooled_unique_ptr &operator=(pooled_unique_ptr &&other)
+    {
+        _ptr = std::move(other._ptr);
+        _ptr_pool_index = other._ptr_pool_index;
+        other._ptr = nullptr;
+        return *this;
+    }
+
+    // overloaded arrow operator
+    T *operator->()
+    {
+        return _ptr;
+    }
+
+    // overloaded dereference operator
+    T &operator*()
+    {
+        return *_ptr;
+    }
+
+    static void initialize_pool()
+    {
+        _pool_pos = 0;
+        _pool_slots.reset();
+        _initialized = true;
+    }
+};
+
+template <typename T>
+size_t pooled_unique_ptr<T>::find_open_slot()
+{
+    /**
+     * @brief Find an open slot in the pool.
+     * First uses all slots in order 0..MAX_POOL_SIZE-1
+     * Once all slots are used, we rely on the _pool_open_slots set
+     * @throws std::runtime_error indicates that the pool is full if there are no open slots
+     * @return The index of the open slot.
+     */
+    size_t pos = _pool_pos;
+    if (_pool_pos < MAX_POOL_SIZE)
+    {
+        pos = this->_pool_pos;
+        ++this->_pool_pos;
+    }
+    else
+    {
+        if (this->_pool_open_slots.empty())
+        {
+            throw std::runtime_error("ERROR: Memory pool is full");
+        }
+        else
+        {
+            pos = *(this->_pool_open_slots.begin());
+            this->_pool_open_slots.erase(pos);
+        }
+    }
+    return pos;
+}
---
+++ test_pooled_unique_ptr.cpp
@@ -0,0 +1,90 @@
+#include "pooled_unique_ptr.h"
+
+#include <cassert>
+#include <iostream>
+#include <memory>
+#include <string>
+#include <vector>
+
+struct MyObj
+{
+    int item;
+    std::string msg;
+    MyObj(int t, std::string msg) : item(t), msg(msg) {}
+    void greet()
+    {
+        std::cout << "(" << item << ") MyObj: " << msg << std::endl;
+    }
+};
+
+void test_pool_alloc_dealloc()
+{
+    std::cout << "*** test_pool_alloc_dealloc ***" << std::endl;
+    pooled_unique_ptr<MyObj> p1(1, "hello");
+    pooled_unique_ptr<MyObj> p2(2, "world");
+    { // let's create p3 in a different scope
+        pooled_unique_ptr<MyObj> p3(3, "!");
+    }
+    pooled_unique_ptr<MyObj> p4(4, "!");
+    pooled_unique_ptr<MyObj> p5(5, "!"); // this should still work since p3 went out of scope
+    
+    // check that the pool is working
+    bool catch_exception = false;
+    try
+    {
+        pooled_unique_ptr<MyObj> p5(5, "!");
+    }
+    catch (std::runtime_error &e)
+    {
+        catch_exception = true;
+        std::cout << e.what() << std::endl;
+    }
+    assert(catch_exception && "check if the exception is thrown");
+}
+
+void test_pool_move() {
+    std::cout << "*** test_pool_move ***" << std::endl;
+    pooled_unique_ptr<MyObj> p1(1, "hello");   
+    pooled_unique_ptr<MyObj> p2 = std::move(p1);
+
+    p2->greet(); // should be (1) MyObj: hello
+    // assert(p1 == nullptr && "p1 should be nullptr");
+
+    pooled_unique_ptr<MyObj> p3(3, "!");
+    pooled_unique_ptr<MyObj> p4;
+    p4 = std::move(p3);
+    p4->greet();
+    assert(p4->item == 3); // should be (3) MyObj: !
+
+    // the following compile, but will produce a runtime error
+    // test with ASAN
+    // p1->greet();
+    // p3->greet();
+
+
+    pooled_unique_ptr<MyObj> p5 = pooled_unique_ptr<MyObj>(5, "!");
+    p5->greet();
+
+    pooled_unique_ptr<MyObj> p6 = pooled_unique_ptr<MyObj>(6, "!");
+    p6->greet();
+
+    // check that the pool is working
+    bool catch_exception = false;
+    try
+    {
+        pooled_unique_ptr<MyObj> p5(5, "!");
+    }
+    catch (std::runtime_error &e)
+    {
+        catch_exception = true;
+        std::cout << e.what() << std::endl;
+    }
+    assert(catch_exception && "check if the exception is thrown");
+}
+
+int main()
+{
+    test_pool_alloc_dealloc();
+    test_pool_move();
+    return 0;
+}
